3e82e39b135c6ac39197aa4c63152dd6abcad42f
Kay Lannen <kay_lannen@comcast.net>
2017-08-09 11:17:51 -0600

    Added Ross's project, Latex project, and WPF Hello World! for a flow document.

---
+++ WpfApp1/BeyondInfinity/Problem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BeyondInfinity
+{
+    abstract class Problem
+    {
+        public abstract void createProblemGraphic(Grid problemGrid);
+        public abstract void createAnswerGraphic(Grid answerGrid);
+    }
+}
---
+++ WpfApp1/BeyondInfinity/Resistor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondInfinity
+{
+    class Resistor
+    {
+        public Resistor(double resistance)
+        {
+            if (resistance <= 0.0)
+                throw new Exception("Resistance must be positive");
+            _resistance = resistance;
+            _current = 0.0;
+            _voltageDrop = 0.0;
+            _power = 0.0;
+        }
+        private double _resistance;
+        public double Resistance
+        {
+            get { return _resistance; }
+            set { _resistance = value; }
+        }
+
+        private double _current;
+        public double Current
+        {
+            get { return _current; }
+            set { _current = value; _voltageDrop = _resistance * _current; _power = _voltageDrop * _current; }
+        }
+
+        private double _voltageDrop;
+        public double VoltageDrop
+        {
+            get { return _voltageDrop; }
+            set { _voltageDrop = value; _current = _voltageDrop / _resistance; _power = _voltageDrop * _current; }
+        }
+
+        private double _power;
+        public double Power
+        {
+            get { return _power; }
+        }
+    }
+}
